280e27b8353128b2d8a56ec6bacb0f123a5ff1a5
Macs <macs@Hassams-MacBook-Pro.local>
2024-10-05 12:37:25 +0300

    improved UI

--- tasks/forms.py
+++ tasks/forms.py
@@ -3,5 +3,5 @@ from django import forms
 # Form for submiting the new task that user will enter
 class TaskForm(forms.Form):
     task = forms.CharField(max_length=64, label="Task Heading")
-    description = forms.CharField(min_length=16, max_length=264, label="Task Description")
+    description = forms.Field()
 
